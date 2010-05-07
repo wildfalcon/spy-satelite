c40957d2355d8b3acd756aaa2e166bcba8ca36a3
Wildfalcon <laurie@wildfalcon.com>
2010-05-07 17:01:58 +0100

    Push message to visitors channel when a visitor is received

---
+++ lib/spy-satelite.rb
@@ -0,0 +1,23 @@
+require 'pusher'
+
+module Rack
+  class SpySatelite
+    def initialize(app, args)
+      
+      puts args.inspect
+      
+      Pusher.app_id = args[:id]
+      Pusher.key    = args[:key]
+      Pusher.secret = args[:secret]
+
+      @app = app       
+    end                
+
+    def call(env)
+      lat = rand(360)
+      long = rand(360)
+      Pusher['visitors'].trigger('new-visit', {:lat => lat, :long => long})
+      @app.call(env)   
+    end                
+  end
+end
